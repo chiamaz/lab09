303d7edf2e98d81c1cc5abc262e653aef348c323
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-20 16:04:49 +0100

    Ex.93: SimpleController.java implemented

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private List<String> list = new ArrayList<>();
+    private String string;
+
+    @Override
+    public void setStringToPrint(String string) {
+        this.string = Objects.requireNonNull(string);
+        list.add(string);
+    }
+
+    @Override
+    public String getStringToPrint() {
+        return this.string;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.list;
+    }
+
+    @Override
+    public void printString() throws IllegalStateException {
+        try {
+            System.out.println(string);
+        } catch (final Exception e) {
+            throw new IllegalStateException();
+        }
+    }
 
 }
